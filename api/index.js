12b98aca517ca79df1112dde5e29e131dd5fb49b
unknown <maxime.soufer@gmail.com>
2022-05-21 20:56:00 -0400

    try to add server to vercel

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const app = require("express")();
+const {v4} = require("uuid");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -21,6 +22,18 @@ const robotSchema = new mongoose.Schema({
 });
 
 const Robot = mongoose.model("robots", robotSchema);
+
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get("/api/item/:slug", (req, res) => {
+  const {slug} = req.params;
+  res.end(`Item: ${slug}`);
+});
 
 app.get("/api/get-all-robots", async (req, res) => {
   let allRobots = await Robot.find({});
